f7e10c7a84825d393e4954a412230868cd0c912f
Debora Fontanella <debora.fontanella@inventsys.ch>
2026-05-22 11:37:19 +0100

    improve copy link

--- src/routes/Shares.tsx
+++ src/routes/Shares.tsx
@@ -350,7 +350,6 @@ export default function Shares() {
                           <svg xmlns="http://www.w3.org/2000/svg" className="w-3.5 h-3.5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
                           </svg>
-                          Copy link
                         </>
                       )}
                     </button>
